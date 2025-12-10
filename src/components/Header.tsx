f567291e547b4b5fe0c1d2d2675f20bf07587f7b
stockizenresearch-sudo <stockizenresearch@gmail.com>
2025-12-10 17:04:30 +0530

    add form

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -29,6 +29,9 @@ export default function Header() {
             City Master
           </Link>
 
+          <Link className="hover:text-orange-600" to="/admin/form2">
+            Form 2
+          </Link>
           <Link className="hover:text-orange-600" to="/admin/industry">
             Industry Master
           </Link>
